efb78e4fd6ef83084cc18bc9ce8606a87761b832
Safeer Ahmad Rana <pakistanireacts9@gmail.com>
2025-06-30 23:32:39 +0500

    Added Nav bar

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -2,7 +2,13 @@ import React from 'react'
 
 const page = () => {
   return (
-    <div className='text-2xl'>Home</div>
+    <div className='text-2xl'>
+      Home
+      <div className='text-lg text-gray-500 mt-2'>
+        This is the root page of your Next.js application.
+
+      </div>
+    </div>
   )
 }
 
